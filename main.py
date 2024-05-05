afb9f853732e21d854ff00398ceaf9c74133156a
Mesbah791 <mesbaharif791@gmail.com>
2024-05-05 15:02:53 +0600

    now okay

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Bank:
         self.loan_given = 0
         self.is_loan_available = True
         self.is_bankrupt = False
-    
+   
     def createAccount(self, name, email, account_type, address):
         account_number = f'{name}{email}'
         self.accounts[account_number] = {
@@ -19,18 +19,20 @@ class Bank:
             "transaction_history": []
             }
         print(f"Account created ,Your Account number =  {account_number}")
-        
-        
+       
+       
     def delete_account(self, account_number):
         if account_number in self.accounts:
             del self.accounts[account_number]
             print("Account deleted ")
         else:
             print("Account does not exist")
-            
+           
     def view_accounts(self):
         for account_number, details in self.accounts.items():
             print(f"Account Number: {account_number}, Name: {details['name']}, Email: {details['email']}, Address: {details['address']}, Account Type: {details['account_type']}")
+
+
 
 
 class User:
@@ -39,18 +41,19 @@ class User:
         self.name = name
         self.email = email
         self.address = address
-    
+   
     def createAccount(self, name, email, address, account_type):
         self.bank.createAccount(name, email, address, account_type)
-    
+   
     def deposit(self, account_number, amount):
-        if self.bank.accounts[account_number]:   
+        if self.bank.accounts[account_number]:  
             self.bank.accounts[account_number]['balance'] += amount
             self.bank.total_available_balance += amount
             print("Money diposited!!")
             self.bank.accounts[account_number]["transaction_history"].append(f"Deposited: {amount}")
         else:
             print("Account does not exist")
+
 
     def withdraw(self, account_number, amount):
         if self.bank.is_bankrupt == False:
@@ -66,14 +69,15 @@ class User:
                 print("Account does not exist")
         else:
             print("The bank is bankrupt")
-    
+   
+
 
     def check_available_balance(self, account_number):
         if self.bank.accounts[account_number]:
             print(f'Balance: {self.bank.accounts[account_number]['balance']}')
         else:
             print("Account does not exist")
-    
+   
     def check_transaction_history(self, account_number):
         if self.bank.accounts[account_number]:
             print(":::::::Your transaction History:::::")
@@ -81,10 +85,10 @@ class User:
                 print(transaction)
         else:
             print("Account does not exist")
-    
+   
     def take_loan(self, account_number, amount):
-        
-            
+       
+           
             if self.bank.accounts[account_number]['loan_taken'] < 2:
                 self.bank.accounts[account_number]['loan_taken'] += 1
                 self.bank.accounts[account_number]['balance'] += amount
@@ -93,20 +97,25 @@ class User:
                 print("Loan taken successfully")
                 self.bank.accounts[account_number]['transaction_history'].append(f"Loan taken: {amount}")
            
-    
+   
     def transfer_money(self, sender_account_number, receiver_account_number, amount):
-      
+     
         if self.bank.accounts[sender_account_number]['balance'] >= amount:
             self.bank.accounts[sender_account_number]['balance'] -= amount
-    
+   
             self.bank.accounts[receiver_account_number]['balance'] += amount
- 
+
+
             self.bank.accounts[sender_account_number]['transaction_history'].append(f"Transferred: {amount}")
             self.bank.accounts[receiver_account_number]['transaction_history'].append(f"Received: {amount}")
             print(f"Successfully Transferred to {receiver_account_number}")
         else:
             print("Balance is not sufficient")
  
+
+
+
+
 
 
 
@@ -119,24 +128,31 @@ class Admin:
         self.address = address
 
 
+
+
     def createAccount(self, name, email, address, account_type):
         self.bank.createAccount(name, email, address, account_type)
+
 
     def delete_account(self, account_number):
         if self.bank.accounts[account_number]:
             self.bank.delete_account(account_number)
         else:
             print("Account does not exist")
-    
+   
     def view_accounts(self):
         self.bank.view_accounts()
 
-    
+
+   
     def loan_given(self):
         print(f'Total loan given by the bank: {self.bank.loan_given}')
 
+
     def check_total_balance(self):
         print(f'Total available balance of the bank: {self.bank.total_available_balance}')
+
+
 
 
     def loan_on_of(self):
@@ -153,6 +169,8 @@ class Admin:
             print('Invalid choice')
 
 
+
+
 class Main:
     bank = Bank('my bank')
     while True:
@@ -161,7 +179,7 @@ class Main:
             print("2. Admin")
             print("3. Exit")
             choice = int(input("Enter your choice : "))
-            
+           
             if choice == 1:
                 name = input("Enter Your Name : ")
                 email = input("Enter Your Email : ")
@@ -188,6 +206,7 @@ class Main:
                         account_type = input("Your account type please : ")
                         user.createAccount(name, email, address, account_type)
 
+
                     elif choice == 2:
                         account_number = input('Your  account number please : ')
                         user_account = user.bank.accounts.get(account_number,None)
@@ -196,6 +215,7 @@ class Main:
                         else:
                             amount = int(input('Enter  the ammount: '))
                             user.deposit(account_number, amount)
+
 
                     elif choice == 3:
                         account_number = input('Your  account number please : ')
@@ -206,6 +226,7 @@ class Main:
                             amount = int(input('Enter the amount: '))
                             user.withdraw(account_number, amount)
 
+
                     elif choice == 4:
                         account_number = input('Your  account number please : ')
                         user_account = user.bank.accounts.get(account_number,None)
@@ -213,6 +234,7 @@ class Main:
                             print("Account does not exist ^_^")
                         else :
                             user.check_available_balance(account_number)
+
 
                     elif choice == 5:
                         account_number = input('Your  account number please : ')
@@ -222,35 +244,40 @@ class Main:
                         else:
                             user.check_transaction_history(account_number)
 
+
                     elif choice == 6:
                         if user.bank.is_loan_available == True:
                             account_number = input('Your  account number please : ')
-                            
+                           
                             user_account = user.bank.accounts.get(account_number,None)
                             if user_account is None:
                                 print("Account does not exist ^_^")
                             else:
                                 amount = int(input('Enter the amount : '))
                                 user.take_loan(account_number, amount)
-
+                        else:
+                            print("Loan feature is not available")
                     elif choice == 7:
                         sender_account_number = input('Enter your account number: ')
                         sender_user_account = user.bank.accounts.get(sender_account_number,None)
                         receiver_account_number = input('Enter receiver account number: ')
                         receiver_user_account = user.bank.accounts.get(receiver_account_number,None)
-                        
-                        
+                       
+                       
                         if sender_user_account is None or receiver_user_account is None:
                             print("Account does not exist ^_^")
                         else:
                             amount = int(input('Enter the amount: '))
                             user.transfer_money(sender_account_number, receiver_account_number, amount)
 
+
                     elif choice == 8:
                         break
-                    
+                   
                     else:
                         print("Invalid Input!!")
+
+
 
 
             elif choice == 2:
@@ -262,7 +289,7 @@ class Main:
                 admin.createAccount(name, email, address, account_type)
                 while True:
                     print(f"What you are looking for??")
-                    
+                   
                     print("1. Delete account")
                     print("2. View accounts")
                     print("3. Check bank total available balance")
@@ -270,7 +297,8 @@ class Main:
                     print("5. Turn on or off loan")
                     print("6. Exit")
                     choice = int(input("Enter Your Choice : "))
-                    
+                   
+
 
                     if choice == 1:
                         account_number = input('Enter your account number: ')
@@ -280,28 +308,37 @@ class Main:
                         else:
                             admin.delete_account(account_number)
 
+
                     elif choice == 2:
                         admin.view_accounts()
+
 
                     elif choice == 3:
                         admin.check_total_balance()
 
+
                     elif choice == 4:
                         admin.loan_given()
+
 
                     elif choice == 5:
                         admin.loan_on_of()
 
+
                     elif choice == 6:
                         break
 
+
                     else:
                         print("Invalid Input")
+
 
             elif choice == 3:
                 break
             else:
                 print("Invalid Input")    
-        
+       
+
 
 Main()
+
